4a0934c115192df7e2e779a298309baf5504c27b
Gabriel Carpenedo <cbgabe@gmail.com>
2015-03-09 01:09:41 -0300

    Improves production URL

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://gabrielcarpenedo.com',
         mail: {},
         fileStorage: false,
         database: {
